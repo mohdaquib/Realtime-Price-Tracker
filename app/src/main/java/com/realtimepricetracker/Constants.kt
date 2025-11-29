94e250c09e9245f5b578f23976c49545a29a9d5e
mohdaquib <mohd.aquib09@gmail.com>
2025-11-29 20:09:07 +0800

    Project setup with dependencies and constants

---
+++ app/src/main/java/com/realtimepricetracker/Constants.kt
@@ -0,0 +1,10 @@
+package com.realtimepricetracker
+
+object Constants {
+    val STOCK_SYMBOLS = listOf(
+        "AAPL", "GOOG", "TSLA", "AMZN", "MSFT", "NVDA", "META", "NFLX", "ADBE", "CRM",
+        "INTC", "AMD", "ORCL", "CSCO", "IBM", "QCOM", "AVGO", "TXN", "MU", "AMAT",
+        "LRCX", "KLAC", "NOW", "SNPS", "CDNS"
+    )
+    const val WS_URL = "wss://ws.postman-echo.com/raw"
+}
